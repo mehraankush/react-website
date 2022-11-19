80df516cb7e1d5500d9618473fc8f72417f3830b
ankush <mehraankush36@gmail.com>
2022-11-20 00:16:50 +0530

    modified SocialMedia.jsx

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import {BsTwitter,BsInstagram} from 'react-icons/bs'
 import {FaFacebook} from 'react-icons/fa'
-
+import {SiCodechef} from 'react-icons/si';
+import {AiFillGithub} from 'react-icons/ai';
 const SocialMedia = () => {
   return (
     <div className='app__social'>
            <div>
-              <BsInstagram/>
+                <a href={'https://www.instagram.com/mehra_69/'}  target={'_blank'}><BsInstagram/></a> 
+           </div>
+               
+           <div>
+             <a href={'https://github.com/mehraankush' } target={'_blank'}><AiFillGithub /></a> 
            </div>
            <div>
-              <BsTwitter/>
-           </div>
-           <div>
-              <FaFacebook/>
+              <a href={'https://www.codechef.com/users/mehra_69'} target={'_blank'}><SiCodechef/></a>
            </div>
     </div>
   )
